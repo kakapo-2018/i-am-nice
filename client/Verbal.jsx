e5afd601c2e4e3f52c17a487088b4d08a5d26ff0
harrison@devacademy.co.nz <harrison-symes>
2018-08-30 20:01:20 +1200

    solved issue with master

--- client/Verbal.jsx
+++ client/Verbal.jsx
@@ -1,13 +1,5 @@
 import React from 'react'
 
-<<<<<<< HEAD
-export default function Verbal (props) {
-  return (
-    <div>
-      <h1 className="title">Don't insult me</h1>
-    </div>
-  )
-=======
 // export default function Verbal (props) {
 //   return (
 //     <div>
@@ -15,8 +7,6 @@ export default function Verbal (props) {
 //     </div>
 //   )
 // }
-
-
 
 export default class Verbal extends React.Component {
   constructor(props) {
@@ -35,7 +25,7 @@ export default class Verbal extends React.Component {
   onSubmit (e) {
     e.preventDefault() //dont do the default
     const insult = this.state
-    this.props.abuse(insult) //send the cat up to App.jsx
+    this.props.abuse(insult) //send the insult up to App.jsx
   }
   render() {
     return (
@@ -45,5 +35,4 @@ export default class Verbal extends React.Component {
       </form>
     )
   }
->>>>>>> 429c8dd4766d318aa2918cde2aeb1445c50129f8
 }
